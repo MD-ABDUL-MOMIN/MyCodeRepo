01526f090f1f09d9f46e4aba8d7035e18cb1857d
MD-ABDUL-MOMIN <momin.ice16@gmail.com>
2018-08-30 21:50:55 +0600

    folder is added

---
+++ MachineLearning/gdpStat/gdp.py
@@ -0,0 +1,11 @@
+import matplotlib
+
+import matplotlib.pyplot as pyp
+import numpy as np
+import pandas as pd
+import sklearn
+
+#Load the data
+
+url = 'http://bit.ly/kaggletrain'
+train = pd.read_csv(url)
